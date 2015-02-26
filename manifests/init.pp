a01d95a88aea0cf7e8a3f09413b69152957967e4
Travis Thomsen <tthomsen@binaryvoid.com>
2015-02-26 10:00:35 -0600

    remove sudo::conf because it blows up vagrant

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,21 +34,4 @@ class privileges {
     home       => '/home/tthomsen',
     password   => '$6$J6HgYJ4bzT52qyG/$2NpTsujTqLrqh4M6gRFj8fI6U76C5qs2vsj6jsK3PBEmD6MntHPh7JyemFRTO5Ms6SxnFs8E7mlGrUwe5FiIn/'
   }
-  
-  ::sudo::conf { 'admins':
-    ensure  => present,
-    content => '%admin ALL=(ALL) ALL',
-  }
-  
-  sudo::conf { 'wheel':
-    ensure  => present,
-    content => '%wheel ALL=(ALL) ALL',
-  }
- 
-  if $::boardproductname == 'VirtualBox' { 
-    sudo::conf { 'vagrant':
-      ensure  => present,
-      content => '%vagrant ALL=(ALL) NOPASSWD: ALL',
-    }
-  }
 }
